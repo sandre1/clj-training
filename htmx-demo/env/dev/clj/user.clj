042aea0dbf628235d7a495203d334efc59ada425
Andrei Stan <andrei.stan@netdava.com>
2022-11-02 22:32:19 +0200

    Started bulk-update example

--- htmx-demo/env/dev/clj/user.clj
+++ htmx-demo/env/dev/clj/user.clj
@@ -1,17 +1,25 @@
 (ns user
   "Userspace functions you can run by default in your local REPL."
   (:require
-    [clojure.pprint]
-    [clojure.spec.alpha :as s]
-    [clojure.tools.namespace.repl :as repl]
-    [criterium.core :as c]                                  ;; benchmarking
-    [expound.alpha :as expound]
-    [integrant.core :as ig]
-    [integrant.repl :refer [clear go halt prep init reset reset-all]]
-    [integrant.repl.state :as state]
-    [kit.api :as kit]
-    [lambdaisland.classpath.watch-deps :as watch-deps]      ;; hot loading for deps
-    [nas.htmx-demo.core :refer [start-app]]))
+   [clojure.pprint]
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl :as repl]
+   [criterium.core :as c]                                  ;; benchmarking
+   [expound.alpha :as expound]
+   [integrant.core :as ig]
+   [integrant.repl :refer [clear go halt prep init reset reset-all]]
+   [integrant.repl.state :as state]
+   [kit.api :as kit]
+   [lambdaisland.classpath.watch-deps :as watch-deps]      ;; hot loading for deps
+   [nas.htmx-demo.core :refer [start-app]]
+   [clojure.tools.trace :as trace]
+   [portal.api :as p]))
+
+;; (def p (p/open)) ; Open a new inspector
+
+;; or with an extension installed, do:
+(def p (p/open {:launcher :vs-code}))  ; jvm / node only
+(add-tap #'p/submit) ; Add portal as a tap> target
 
 ;; uncomment to enable hot loading for deps
 (watch-deps/start! {:aliases [:dev :test]})
@@ -46,10 +54,19 @@
 (comment
   (go)
   (reset)
-  
+  (trace/trace-ns nas.htmx-demo.web.routes.ui)
+  (trace/trace-vars nas.htmx-demo.web.routes.ui/edit)
 
   (kit/sync-modules)
   (kit/list-modules)
   (kit/install-module :kit/html)
   (kit/install-module :kit/htmx)
+
+  (tap> {:name "nas"})
+  (prn (type @p))
+  (prn (:body (first @p)))
+  (def a @p)
+  (let [f1 (first a)]
+    a)
+
   )
--- htmx-demo/src/clj/nas/htmx_demo/web/routes/ui.clj
+++ htmx-demo/src/clj/nas/htmx_demo/web/routes/ui.clj
@@ -14,7 +14,8 @@
     [:meta {:charset "UTF-8"}]
     [:title "Htmx + Kit"]
     [:script {:src "https://unpkg.com/htmx.org@1.7.0/dist/htmx.min.js" :defer true}]
-    [:script {:src "https://unpkg.com/hyperscript.org@0.9.5" :defer true}]]
+    [:script {:src "https://unpkg.com/hyperscript.org@0.9.5" :defer true}]
+    [:link {:href "/css/htmx-styles.css" :rel "stylesheet" :type "text/css"}]]
    [:body
     [:div {:hx-target "this" :hx-swap "outerHTML"}
      [:div [:label "First Name"] "Joe"]
@@ -31,8 +32,13 @@
    [:div "Congratulations! You just clicked the button!"]))
 
 (defn edit [request]
+  #_(let [b (slurp (:body request))]
+    (tap> b))
+  ;; (tap> request)
+  (let [{:keys [form-params]} request]
+    (tap> form-params))
   (ui
-   [:form {:hx-put "/"
+   [:form {:hx-post "/edit"
            :hx-target "this"
            :hx-swap "outerHTML"}
     [:div [:label "First Name"]
@@ -46,12 +52,109 @@
     [:button {:class "btn"} "Submit"]
     [:button {:class "btn" :hx-get "/"} "Cancel"]]))
 
+(defn post-edit [request]
+  (let [{:keys [form-params]} request
+        {:strs [firstName lastName email]} form-params]
+    (tap> form-params)
+    (ui
+     [:div {:hx-target "this" :hx-swap "outerHTML"}
+     [:div [:label "First Name"] firstName]
+     [:div [:label "Last Name"] lastName]
+     [:div [:label "Email"] email]
+     [:button {:hx-get "/edit" :class "btn btn-primary"} "Click to edit"]])))
+
+(def bulk-data [{:name "Stan Andrei" :email "stan.andrei@gmail.com" :status "Active" :index "0"}{:name "Joe Smith" :email "joe@smith.org" :status "Active" :index "1"} {:name "Angie MacDowell" :email "	angie@macdowell.org" :status "Active" :index "2"} {:name "Fuqua Tarkenton" :email "fuqua@tarkenton.org" :status "Active" :index "3"} {:name "Kim Yee" :email "	kim@yee.org" :status "Inactive" :index "4"}])
+
+(defn in?
+      "true if coll contains elm"
+      [coll elm]
+      (let [v (into [] coll)]
+        (some #(= elm %) coll)))
+
+(defn bulk-row [person]
+  (let [name (:name person)
+        email (:email person)
+        status (:status person)
+        index (:index person)]
+    [:tr {:class ""}
+     [:td [:input {:type "checkbox" :name "ids" :value index}]]
+     [:td name]
+     [:td email]
+     [:td status]]))
+
+(defn bulk-activate-row [person ids]
+  (let [name (:name person)
+        email (:email person)
+        status (:status person)
+        index (:index person)
+        active? (in? ids index)]
+    [:tr {:class (if active? "activate" "")}
+         [:td [:input {:type "checkbox" :name "ids" :value index}]]
+         [:td name]
+         [:td email]
+         [:td (if active? "Active" status)]]))
+
+(defn bulk-rows [persons]
+  (map bulk-row persons))
+
+(defn bulk-activate-rows [persons ids]
+  (map #(bulk-activate-row % ids) persons))
+
+(defn bulk-update [request]
+  (page
+   [:head
+    [:meta {:charset "UTF-8"}]
+    [:title "Htmx + Kit"]
+    [:script {:src "https://unpkg.com/htmx.org@1.7.0/dist/htmx.min.js" :defer true}]
+    [:script {:src "https://unpkg.com/hyperscript.org@0.9.5" :defer true}]
+    [:link {:href "/css/htmx-styles.css" :rel "stylesheet" :type "text/css"}]]
+   [:body
+    [:form {:id "checked-contacts"}
+     [:table
+      [:thead [:tr [:th]
+               [:th "Name"]
+               [:th "Email"]
+               [:th "Status"]]
+       [:tbody {:id "tbody"}
+        (bulk-rows bulk-data)]]]]
+    [:div {:hx-include "#checked-contacts" :hx-target "#tbody"}
+     [:a {:class "btn" :hx-put "/bulk-update/activate"} "Activate"]
+     [:a {:class "btn" :hx-put "/bulk-update/deactivate"} "Deactivate"]]]
+   ))
+
+(defn activate [request]
+  (let [ids (:form-params request)]
+    (ui
+   [:head
+    [:meta {:charset "UTF-8"}]
+    [:title "Htmx + Kit"]
+    [:script {:src "https://unpkg.com/htmx.org@1.7.0/dist/htmx.min.js" :defer true}]
+    [:script {:src "https://unpkg.com/hyperscript.org@0.9.5" :defer true}]
+    [:link {:href "/css/htmx-styles.css" :rel "stylesheet" :type "text/css"}]]
+   [:body
+    [:form {:id "checked-contacts"}
+     [:table
+      [:thead [:tr [:th]
+               [:th "Name"]
+               [:th "Email"]
+               [:th "Status"]]
+       [:tbody {:id "tbody"}
+        (bulk-activate-rows bulk-data ids)]]]]])))
+
+;; (defn activate [request]
+;;   [:div "bulk activate"])
+(defn deactivate [request]
+  [:div "bulk deactivate"])
+
 ;; Routes
 (defn ui-routes [_opts]
-  [["/" {:get home}]
+  [["/" home]
    ["/clicked" {:post clicked}]
    ["/edit" {:get edit
-             :put home}]])
+             :post post-edit}]
+   ["/bulk-update" {:get bulk-update}]
+   ["/bulk-update/activate" {:put activate}]
+   ["/bulk-update/deactivate" {:put deactivate}]])
 
 (defn route-data [opts]
   (merge
@@ -72,3 +175,12 @@
       :or   {base-path ""}
       :as   opts}]
   [base-path (route-data opts) (ui-routes opts)])
+
+(comment 
+  (let [ids ["0" "1"]]
+    (contains? ids 1))
+  (in? '("1") "1")
+  (into [] ["1" "2"])
+  (bulk-rows bulk-data)
+  (activate {:form-params "4"})
+  0)
