cac239246db30471560c3949f775fab2b5cd129a
Andrei Stan <andrei.stan@netdava.com>
2023-01-26 21:31:17 +0200

    Added style for lazy-loading

--- htmx-demo/src/clj/nas/htmx_demo/htmx_examples/lazy_loading.clj
+++ htmx-demo/src/clj/nas/htmx_demo/htmx_examples/lazy_loading.clj
@@ -12,6 +12,25 @@
    [:body
     [:div {:class "container"}
      [:div {:class "example-wrapper"}
+      [:h2 "Lazy Loading"]
+      [:p "This example shows how to lazily load an element on a page. We start with an initial state that looks like this:"]
+      
+      [:pre [:code {:class "language-html"}
+             "   [:div {:hx-get \"lazy-loading/graph\"
+             :hx-trigger \"load\"}
+       [:img {:alt \"Result loading...\"
+              :class \"htmx-indicator\"
+              :width \"150\"
+              :src \"/img/bars.svg\"}]]"]]
+      
+      [:p "Which shows a progress indicator as we are loading the graph. The graph is then loaded and faded gently into view via a settling CSS transition:"]
+      
+      [:pre [:code {:class "language-html"}
+             "   [:style \".htmx-settling img {opacity: 0;}
+            img {transition: opacity 300ms ease-in;}\"]"]]
+      
+      [:h2 "Demo"]
+      
       [:div {:hx-get "lazy-loading/graph"
              :hx-trigger "load"}
        [:img {:alt "Result loading..."
