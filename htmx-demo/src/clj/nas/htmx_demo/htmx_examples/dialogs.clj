09c0d838c7bb735ac14c8fc0037900d9b5e1cb50
Andrei <sandre1>
2023-02-02 23:15:09 +0200

    Added text and style for dialog example

--- htmx-demo/src/clj/nas/htmx_demo/htmx_examples/dialogs.clj
+++ htmx-demo/src/clj/nas/htmx_demo/htmx_examples/dialogs.clj
@@ -9,10 +9,31 @@
     [:title "Htmx + Kit"]
     [:script {:src "https://unpkg.com/htmx.org@1.7.0/dist/htmx.min.js"}]
     [:script {:src "https://unpkg.com/hyperscript.org@0.9.5"}]
-    #_[:link {:href "/css/htmx-styles.css" :rel "stylesheet" :type "text/css"}]]
+    [:link {:href "/css/htmx-styles.css" :rel "stylesheet" :type "text/css"}]]
    [:body
     [:div {:class "container"}
      [:div {:class "example-wrapper"}
+      [:h2 "Dialogs"]
+      [:p "Dialogs can be triggered with the " [:code "hx-prompt"] " and " [:code "hx-confirm"] " attributes. These are triggered by the user interaction that would trigger the AJAX request, but the request is only sent if the dialog is accepted."]
+      
+      [:pre
+       [:code {:class "language-html"}
+        "[:div
+ [:button {:class \"btn\"
+           :hx-post \"/dialogs/submit\"
+           :hx-prompt \"Enter a string\"
+           :hx-confirm \"Are you sure?\"
+           :hx-target \"#response\"}
+  \"Prompt Submission\"]
+ [:div {:id \"response\"}]]"]]
+      
+      [:p "The value provided by the user to the prompt dialog is sent to the server in a " [:code "HX-Prompt"] " header. In this case, the server simply echos the user input back."]
+      
+      [:pre [:code {:class "language-html"}
+             "[:div \"User entered\" [:i prompt-text]]"]]
+      
+      [:h2 "Demo"]
+      
       [:div
        [:button {:class "btn"
                  :hx-post "/dialogs/submit"
@@ -27,4 +48,4 @@
   (ui
    (let [headers (:headers request)
          prompt-text (get headers "hx-prompt")]
-     [:div prompt-text])))
+     [:div "User entered " [:i prompt-text]])))
