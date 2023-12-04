7a713c16882e7de5523d54c36a92f9449fd222d6
Andrei <sandre1>
2023-12-04 13:25:30 +0200

    Modified code for ynchronized methods

--- java-concurrency/src/concurrency/deadlock.clj
+++ java-concurrency/src/concurrency/deadlock.clj
@@ -3,30 +3,29 @@
 
 (defprotocol Friend
   (get-name [this])
-  (bow [this bower])
-  (bow-back [this bower]))
+  (bow [this bower lock])
+  (bow-back [this bower lock]))
 
-(def p (Object.))
+
 
 (defrecord FriendImplement [name]
   Friend
   (get-name [_] name)
-  (bow [this bower]
-       (printf "%s: %s has bowed to me!%n" (get-name this) (get-name bower))
-       (.start (Thread. #(bow-back this bower))))
-  (bow-back [this bower]
-    (printf "%s: %s has bowed back to me!" (get-name this) (get-name bower))))
+  (bow [this bower lock]
+       (locking lock
+         (printf "%s: %s has bowed to me!%n" (get-name this) (get-name bower))
+         (.start (Thread. #(bow-back this bower lock)))))
+  (bow-back [this bower lock]
+    (locking lock
+      (printf "%s: %s has bowed back to me!" (get-name this) (get-name bower)))))
 
 
 (defn -main [& args]
   (let [alphonse (->FriendImplement "Alphonse")
-        gaston (->FriendImplement "Gaston")]
-    (future
-      (locking p
-        (.start (Thread. #(bow alphonse gaston)))))
-    (future (locking p
-              (.start (Thread. #(bow gaston alphonse)))))
-    (println "abcd")))
+        gaston (->FriendImplement "Gaston")
+        lock (Object.)]
+    (.start (Thread. #(bow alphonse gaston lock)))
+    (.start (Thread. #(bow gaston alphonse lock)))))
 
 
 (comment 
